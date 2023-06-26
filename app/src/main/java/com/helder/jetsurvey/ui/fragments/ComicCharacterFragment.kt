9e4b08260d7fdf79d8d6df0efec41db91eaef5ab
helderzack <helderzacarias@outlook.com>
2023-06-26 18:00:51 -0300

    feat: Interface of the second survey question without the images of the comic characters in the radio buttons.
    feat: Basic navigation flow between all the fragments.

--- app/src/main/java/com/helder/jetsurvey/ui/fragments/ComicCharacterFragment.kt
+++ app/src/main/java/com/helder/jetsurvey/ui/fragments/ComicCharacterFragment.kt
@@ -7,10 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.helder.jetsurvey.R
-import com.helder.jetsurvey.databinding.FragmentFreeTimeActivitiesBinding
+import com.helder.jetsurvey.databinding.FragmentComicCharacterBinding
 
-class FreeTimeActivitiesFragment: Fragment(R.layout.fragment_free_time_activities) {
-    private lateinit var binding: FragmentFreeTimeActivitiesBinding
+class ComicCharacterFragment: Fragment(R.layout.fragment_comic_character) {
+    private lateinit var binding: FragmentComicCharacterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,15 +18,14 @@ class FreeTimeActivitiesFragment: Fragment(R.layout.fragment_free_time_activitie
         savedInstanceState: Bundle?
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
-        binding = FragmentFreeTimeActivitiesBinding.inflate(layoutInflater)
+        binding = FragmentComicCharacterBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.materialButtonNext.setOnClickListener {
-            val action = FreeTimeActivitiesFragmentDirections
-                .actionFreeTimeActivitiesFragmentToComicCharacterFragment()
+        binding.materialButtonPrevious.setOnClickListener {
+            val action = ComicCharacterFragmentDirections.actionComicCharacterFragmentToFreeTimeActivitiesFragment()
             findNavController().navigate(action)
         }
     }
